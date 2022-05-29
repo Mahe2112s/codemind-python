b81ead57ca1564f010a4dec2dd561577f769153e
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-05-29 20:13:13 +0530

    n=int(input()) d=0 p=0 s=0 while n!=0:     r=n%10     n=n//10     d=d*10+r while d!=0:     f=d%10     if f==6 and s==0:         f=9         s+=1     p=p*10+f     d=d//10 print(p)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,16 +1,17 @@
 n=int(input())
-s=0
 d=0
-l=0
+p=0
+s=0
 while n!=0:
     r=n%10
     n=n//10
     d=d*10+r
 while d!=0:
-    p=d%10
-    if p==6 and s==0:
-        p=9
+    f=d%10
+    if f==6 and s==0:
+        f=9
         s+=1
-    l=l*10+p
+    p=p*10+f
     d=d//10
-print(l)
+print(p)
+    
