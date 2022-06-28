9f48a2d56f503d45005fe1b318bce737a4a1e7a0
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-06-28 19:09:00 +0530

    Update Unique_number.py

--- Unique_number.py
+++ Unique_number.py
@@ -1,22 +1,16 @@
-n=input()
-s=[]
-d=len(n)
-for i in n:
-    if i not in s:
-        s.append(i)
-if(len(s)==d):
-    print('Unique Number')
+def convert(n):
+    p=[int(i) for i in str(n)]
+    return p
+n=int(input())
+d=n
+l=len(str(d))
+c=0
+a=convert(n)
+for i in range(0,l):
+    for j in range(0,i):
+        if a[i]==a[j]:
+            c+=1
+if(c==0):
+    print("Unique Number")
 else:
-    print('Not Unique Number')
-    
-2nd Method 
-n=input()
-s=[]
-d=len(n)
-for i in n:
-    if i not in s:
-        s.append(i)
-if(len(s)==d):
-    print('Unique Number')
-else:
-    print('Not Unique Number')
+    print("Not Unique Number")
