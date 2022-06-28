e6fd2e8fb7f700f2dd03c69dba5c767d19363c21
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-06-28 19:07:04 +0530

    Update Unique_number.py

--- Unique_number.py
+++ Unique_number.py
@@ -8,3 +8,15 @@ if(len(s)==d):
     print('Unique Number')
 else:
     print('Not Unique Number')
+    
+2nd Method 
+n=input()
+s=[]
+d=len(n)
+for i in n:
+    if i not in s:
+        s.append(i)
+if(len(s)==d):
+    print('Unique Number')
+else:
+    print('Not Unique Number')
