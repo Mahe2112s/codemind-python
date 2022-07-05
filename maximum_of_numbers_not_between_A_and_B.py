256048f967c72a62771806b8e35f2bc7244d1246
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-07-05 15:30:55 +0530

    n=int(input()) x=list(map(int,input().split())) a,b=map(int,input().split()) f=0 s=[] for i in x:     if i not in range(a,b+1):         s.append(i)         f=1 if(f==0):     print('-1') else:     print(max(s))

---
+++ maximum_of_numbers_not_between_A_and_B.py
@@ -0,0 +1,13 @@
+n=int(input())
+x=list(map(int,input().split()))
+a,b=map(int,input().split())
+f=0
+s=[]
+for i in x:
+    if i not in range(a,b+1):
+        s.append(i)
+        f=1
+if(f==0):
+    print('-1')
+else:
+    print(max(s))
