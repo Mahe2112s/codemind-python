d93633c7f816b151f85d514079f0b1b633507a96
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-07-15 21:33:34 +0530

    def dig(n):     c=0     while(n!=0):         r=n%10         c+=r         n=n//10     return c n=int(input()) x=list(map(int,input().split())) s=[] c=0 for i in x:     s.append(dig(i)) print(sum(s))

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,11 +1,15 @@
-def fck(n):
-    s=0
-    for i in str(n):
-        s+=int(i)
-    return s
+def dig(n):
+    c=0
+    while(n!=0):
+        r=n%10
+        c+=r
+        n=n//10
+    return c
 n=int(input())
 x=list(map(int,input().split()))
 s=[]
+c=0
 for i in x:
-    s.append(fck(i))
+    s.append(dig(i))
 print(sum(s))
+    
