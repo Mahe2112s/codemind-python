c670ab5cf2a0c269c6dbb3eb278d557bc503c862
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-08-29 14:38:17 +0530

    def string(str):     return str[::-1] n=input().lower() n=n.split() for i in range(len(n)):     if i%2==0:         print(string(n[i]),end=" ")     else:         print(n[i],end=" ")

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,10 +1,9 @@
-def rev(n):
-    a=n[::-1]
-    return a
-n=input()
+def string(str):
+    return str[::-1]
+n=input().lower()
 n=n.split()
 for i in range(len(n)):
     if i%2==0:
-        print(rev(n[i]),end=" ")
+        print(string(n[i]),end=" ")
     else:
         print(n[i],end=" ")
