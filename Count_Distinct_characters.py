5f6eb429bbdbd3848c4aee953bb4925140292866
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-08-29 11:26:50 +0530

    def string(str):     return ''.join(sorted(str)) n=input() n=n.lower() s=[] for i in n:     if i==' ':         continue     else:         if i not in s:             s.append(i) print(len(s))

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,9 +1,13 @@
+def string(str):
+    return ''.join(sorted(str))
 n=input()
-s=[]
 n=n.lower()
+s=[]
 for i in n:
     if i==' ':
-       continue
-    if i not in s:
-        s.append(i)
+        continue
+    else:
+        if i not in s:
+            s.append(i)
 print(len(s))
+        
