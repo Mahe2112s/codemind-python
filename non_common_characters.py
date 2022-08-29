617f2e6ad34a1064b9a6744e021d74dbb556d5d3
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-08-29 11:37:53 +0530

    def string(str):     return ''.join(sorted(str)) n=input().lower() m=input().lower() s=[] for i in n:     if i==' ':         continue     if i not in m and i not in s:         s.append(i) for i in m:     if i==' ':         continue     if i not in n and i not in s:         s.append(i) print(string(s))

---
+++ non_common_characters.py
@@ -0,0 +1,17 @@
+def string(str):
+    return ''.join(sorted(str))
+n=input().lower()
+m=input().lower()
+s=[]
+for i in n:
+    if i==' ':
+        continue
+    if i not in m and i not in s:
+        s.append(i)
+for i in m:
+    if i==' ':
+        continue
+    if i not in n and i not in s:
+        s.append(i)
+print(string(s))
+        
